0e103777a49685e5d11310c43c2319ab8d82b1ac
unknown <victorcdc96@gmail.com>
2018-11-24 22:21:11 -0200

    estudos em Java

---
+++ Fatorial/src/Fatorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+    public static void main(String[] args){
+
+        Scanner ent = new Scanner(System.in);
+        int num, fat = 1;
+        int cont = 1;
+
+        do {
+            System.out.print("Digite um número inteiro : ");
+            num = ent.nextInt();
+
+            for(int i = 1;i <= num; i++){
+                fat = fat * i;
+            }
+
+            System.out.println(num + "! = " + fat);
+            cont++;
+
+        } while(cont < 2);
+    }
+}
